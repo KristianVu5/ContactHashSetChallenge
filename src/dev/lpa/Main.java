3aa31a0734ff035b081cd195f3d38d26ba173e05
Kristian Vu <krisivu5@gmail.com>
2026-09-24 22:43:44 +0300

    symmetric operations on set

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -23,6 +23,35 @@ public class Main {
         robinHood.addEmail("Sherwood Forest");
         robinHood.replaceEmailIfExists("RHood@sherwoodforest.com","RHood@sherwoodforest.org");
         System.out.println(robinHood);
+
+        Set<Contact> unionAB = new HashSet<>();
+        unionAB.addAll(emailContacts);
+        unionAB.addAll(phoneContacts);
+        printData("(A ∪ B) Union of emails (A) with phones (B)", unionAB);
+
+        Set<Contact> intersectAB = new HashSet<>(emailContacts);
+        intersectAB.retainAll(phoneContacts);
+        printData("(A ∩ B) Intersect emails (A) and phones (B)",intersectAB);
+
+        Set<Contact> intersectBA = new HashSet<>(phoneContacts);
+        intersectBA.retainAll(emailContacts);
+        printData("(B ∩ A) Intersect emails (B) and phones (A)",intersectBA);
+
+        Set<Contact> AMinusB = new HashSet<>(emailContacts);
+        AMinusB.removeAll(phoneContacts);
+        printData("(A - B) Intersect emails (A) - phones (B)",AMinusB);
+
+        Set<Contact> BMinusA = new HashSet<>(phoneContacts);
+        BMinusA.removeAll(emailContacts);
+        printData("(B - A) Intersect emails (B) - phones (A)",BMinusA);
+
+        Set<Contact> symmetricDiff = new HashSet<>(AMinusB);
+        symmetricDiff.addAll(BMinusA);
+        printData("Symmetric Difference: phones and emails", symmetricDiff);
+
+        Set<Contact> symmetricDiff2 = new HashSet<>(unionAB);
+        symmetricDiff2.removeAll(intersectAB);
+        printData("Symmetric Difference: phones and emails", symmetricDiff2);
     }
 
     public static void printData(String header, Collection<Contact> contacts) {
